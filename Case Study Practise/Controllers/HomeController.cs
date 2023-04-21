9cd9dece1d7936b07848f6c74713b82269060c77
SWR\syed1 <syed110wasif@gmail.com>
2023-04-22 02:57:42 +0530

    Add project files.

---
+++ Case Study Practise/Controllers/HomeController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Case_Study_Practise.Controllers
+{
+    public class HomeController : Controller
+    {
+        PractiseCaseStudyContext context = new PractiseCaseStudyContext();
+        public ActionResult Index()
+        {
+            var listofData = context.Products.ToList();
+            return View(listofData);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateProduct(Product product)
+        {
+            try
+            {
+                if (product.ID == 0){
+                    Product _products = new Product();
+                    _products.Product_Title = product.Product_Title;
+                    _products.Price = Convert.ToInt32(product.Price);
+                    _products.Stock = Convert.ToInt32(product.Stock);
+                    _products.Image = product.Image;                    
+                    context.Products.Add(_products);
+                    context.SaveChanges();                    
+                }
+                else if(product.ID != 0)
+                {
+                    Product _products = new Product();
+                    var data = context.Products.Where(x => x.ID == product.ID).FirstOrDefault();
+                    data.Product_Title = product.Product_Title;
+                    data.Price = Convert.ToInt32(product.Price);
+                    data.Stock = Convert.ToInt32(product.Stock);
+                    data.Image = product.Image;                    
+                    context.SaveChanges();
+                }
+                return Json(product);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        } 
+
+        [HttpGet]
+        public ActionResult GetAllProd()
+        {
+            try
+            {
+                var allProd = context.Products.ToList();
+                return Json(allProd, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        [HttpPost]
+        public ActionResult DeleteProduct(Product obj)
+        {
+            try
+            {
+                var DeletedProd = context.Products.Remove(context.Products.Where(x => x.ID == obj.ID).FirstOrDefault());
+                context.SaveChanges();
+                return Json(DeletedProd, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ Case Study Practise/Scripts/Custom.js
@@ -0,0 +1,59 @@
+﻿$(document).ready(function () {
+    getallprod();
+
+});
+
+
+function AddProducts() {
+    var product = {
+        ID: $("#hdnId").val(),
+        Product_Title: $("#txtProductTitle").val(),
+        Price: $("#txtPrice").val(),
+        Stock: $("#txtStock").val(),
+        Image: $("#txtImage").val(),
+    }
+
+    $.post("/Home/CreateProduct", product).done(function () {
+        alert("Success");
+        getallprod();
+    }).fail(function (xhr) {
+        console.log(xhr);
+    });
+}
+
+
+function getallprod() {
+    $("#tbl_Products tbody").empty();
+    $.get("/Home/GetAllProd").done(function (result) {
+        /*console.log(result);*/
+        for (var i = 0; i < result.length; i++) {
+            $("#tbl_Products tbody").append('<tr> <td>' + result[i].Product_Title + '</td> <td>' + result[i].Price + '</td> <td>' + result[i].Stock + '</td> <td><a onclick="UpdateEntry(' + result[i].ID + ' ,this)"><i class="fa fa-edit"></i></a> <a onclick="DeleteProduct(' + result[i].ID + ')"><i class="fa fa-trash" aria-hidden="true"></a></i></td> <tr/>')
+        }
+    }).fail(function (xhr) {
+        console.log(xhr);
+    });
+}
+
+function UpdateEntry(ID, e) {
+    var currentRow = $(e).closest("tr");
+    var obj = {
+        ID: ID,
+        Product_Title: currentRow.find("td:eq(0)").text(),
+        Price: currentRow.find("td:eq(1)").text(),
+        Stock: currentRow.find("td:eq(2)").text()
+    }
+    $("#hdnId").val(obj.ID);
+    $("#txtProductTitle").val(obj.Product_Title);
+    $("#txtPrice").val(obj.Price);
+    $("#txtStock").val(obj.Stock);    
+}
+
+function DeleteProduct(ID) {
+    var obj = {
+        ID: ID
+    }
+    $.post("/Home/DeleteProduct", obj).done(function (result) {
+        alert("Deleted");
+        getallprod();
+    })
+}
