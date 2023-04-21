9ccd366b7c65109890d4f327bccb8d6188ddda32
SWR\syed1 <syed110wasif@gmail.com>
2023-04-22 03:20:21 +0530

    Syed - changes

--- Case Study Practise/Scripts/Custom.js
+++ Case Study Practise/Scripts/Custom.js
@@ -1,5 +1,6 @@
 ﻿$(document).ready(function () {
     getallprod();
+    $("#tbl_Products").dataTable();
 
 });
 
@@ -28,7 +29,7 @@ function getallprod() {
         /*console.log(result);*/
         for (var i = 0; i < result.length; i++) {
             $("#tbl_Products tbody").append('<tr> <td>' + result[i].Product_Title + '</td> <td>' + result[i].Price + '</td> <td>' + result[i].Stock + '</td> <td><a onclick="UpdateEntry(' + result[i].ID + ' ,this)"><i class="fa fa-edit"></i></a> <a onclick="DeleteProduct(' + result[i].ID + ')"><i class="fa fa-trash" aria-hidden="true"></a></i></td> <tr/>')
-        }
+        };        
     }).fail(function (xhr) {
         console.log(xhr);
     });
